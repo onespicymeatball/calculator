93d5814b6be0212e16c9f191b067c93542c206bd
onespicymeatball <msmtellez93@gmail.com>
2020-02-21 20:26:54 -0800

    Implement function for "Add"

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 import './style.css';
 
 // Write Javascript code!
-const container = document.getElementById('calculator');
 const add_button = document.getElementById('add');
 const subt_button = document.getElementById('subtract');
 const mult_button = document.getElementById('multiply');
@@ -11,3 +10,9 @@ const num1_hopper = document.getElementById('num1');
 const num2_hopper = document.getElementById('num2');
 const output_hopper = document.getElementById('output')
 
+add_button.addEventListener('click', (event) => {
+  const num1 = +num1_hopper.value;
+  const num2 = +num2_hopper.value;
+  const output = num1 + num2;
+  output_hopper.innerText = output;
+});
